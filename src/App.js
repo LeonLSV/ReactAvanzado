72c0ca1a5a2eb2049c6a29bdd45397fb873d7292
Leon Ramiro <racramiro@gmail.com>
2020-11-19 14:02:12 -0500

    fin Reach ROuter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { Logo } from "./components/Logo";
 import { Home } from "./pages/Home";
 import { Router } from "@reach/router";
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+import { NavBar } from "./components/Navbar";
+import Context from "./Context";
 
 export const App = () => {
   return (
@@ -15,6 +20,24 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+
+      <Context.Consumer>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </Context.Consumer>
+
+      <NavBar />
     </>
   );
 };
