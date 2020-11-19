f2157ddf9cf8a270d27ac88498ec003e948b05ad
Leon Ramiro <racramiro@gmail.com>
2020-11-18 20:06:32 -0500

    Fin Graphql

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
-import { ImgWrapper, Img, Button, Article } from "./styles";
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { ImgWrapper, Img, Article } from "./styles";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
-
+import { FavButton } from "../FavButton";
+import { ToggleLikeMutation } from "../../container/ToggleLikeMutation";
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
@@ -11,22 +11,31 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
   const key = `like-${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
   return (
     <Article ref={element}>
       {show && (
         <>
-          <a href={`/detail/${id}`}>
+          <a href={`/?detail=${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
           </a>
-
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size="32px" />
-            {likes} likes!
-          </Button>
+          <ToggleLikeMutation>
+            {(toggleLike) => {
+              const handleFavClick = () => {
+                !liked && toggleLike({ variables: { input: { id } } });
+                setLiked(!liked);
+              };
+              return (
+                <FavButton
+                  liked={liked}
+                  likes={likes}
+                  onClick={handleFavClick}
+                />
+              );
+            }}
+          </ToggleLikeMutation>
         </>
       )}
     </Article>
