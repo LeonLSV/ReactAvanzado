a4a4d50b34605213d889a4306e396201f0d24517
Leon Ramiro <racramiro@gmail.com>
2020-12-20 17:44:06 -0500

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 // webpack.config.js
+const { pathToArray } = require("graphql/jsutils/Path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const path = require("path");
 
 module.exports = {
   output: {
@@ -9,6 +16,32 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Petgram - App de fotos de mascotas",
+      shortname: "Petgram",
+      description: "Encontraras fotos de animales domésticos muy bonitos",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://petgramm-server-vercel-leon.vercel.app"
+          ),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
